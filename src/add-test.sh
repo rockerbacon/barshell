103a0d980cfe6bea849edb56a5b0ac6ded8da37d
rockerbacon <vitor@librem.one>
2020-06-28 01:34:56 -0300

    Fixed test file creation

--- src/add-test.sh
+++ src/add-test.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-file_path=${1%.sh}; shift
+file_path=$1; shift
 
 file_dir=${project_root:?}/tests/$(dirname "$file_path")
-file_name=${file_path##*/}.sh
+
+raw_file_name=${file_path##*/}
+file_name_without_prefix=${raw_file_name#test_}
+clean_file_name=${file_name_without_prefix%.sh}
+file_name=test_$clean_file_name.sh
+
+depth=$(grep -o "/" <<< "$file_path" | wc -l)
+
+if [ "$depth" -gt 0 ]; then
+	exit_depth=$(bash -c "printf '%.0s\\/..' {1..$depth}")
+fi
 
 mkdir -p "$file_dir"
 
 cp "${module_root:?}/templates/test.sh" "$file_dir/$file_name"
 
 relative_module_root=${module_root#$project_root/}
-escaped_relative_module_root=${relative_module_root/\//\/\/}
+escaped_relative_module_root=${relative_module_root/\//\\\/}
 
-sed -i "s/<%relative_module_root%>/$escaped_relative_module_root/g" "$file_dir/$file_name"
+sed -i \
+	-e "s/<%relative_module_root%>/$escaped_relative_module_root/g" \
+	-e "s/<%exit_depth%>/$exit_depth/g" \
+	"$file_dir/$file_name"
 
--- templates/test.sh
+++ templates/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-project_root=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../..")
+project_root=$(realpath "$(dirname "${BASH_SOURCE[0]}")<%exit_depth%>/..")
 
 oneTimeSetUp() {
 
@@ -14,5 +14,6 @@ test_something() {
 
 }
 
+# shellcheck disable=SC1090
 . "$project_root/<%relative_module_root%>/shunit2/shunit2"
 
